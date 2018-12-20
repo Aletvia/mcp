76a59a3e85209700d296605c38e704de56c8a256
Aletvia <aletvialecona@gmail.com>
2018-12-19 22:13:36 -0600

    Preregistro maqueta

---
+++ application/views/welcome_message.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Microprestamos123</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <link href="http://localhost/Microprestamos123/assets/css/img/favicon.png" rel="icon">
+  <link href="http://localhost/Microprestamos123/assets/css/img/apple-touch-icon.png" rel="microprestamos123">
+
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900|Raleway:400,300,700,900" rel="stylesheet">
+
+  <link href="http://localhost/Microprestamos123/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="http://localhost/Microprestamos123/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+  <link href="http://localhost/Microprestamos123/assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Fixed navbar -->
+  <div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        <a class="navbar-brand" href="https://microprestamos123.com/"><img src="http://localhost/Microprestamos123/assets/css/img/apple-touch-icon.png" alt="Microprestamos123" ></a>
+      </div>
+    </div>
+  </div>
+  
+    <div class="container" >
+	<h1 class="col-lg-8 col-lg-offset-2 col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1  col-xs-8 col-xs-offset-2">
+	Realiza tu Pre registro y disfruta de sus beneficios así como la tasa de interés más baja durante todo el 2019 exclusivamente para nuestros usuarios en etapa de pre registro.</h1>
+<div class="bgwt">
+<h1 class="bgwt col-lg-8 col-lg-offset-2 col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1  col-xs-8 col-xs-offset-2">
+	<br><br>Prestamos hasta $2,000.00 Rápido, fácil y simple.<br>Lanzamiento 19 de Marzo del 2019<br><br><br></h1>
+	</div>
+		<div id="preregister"  style="margin-top:30px;" class="col-lg-10 col-lg-offset-1 col-md-12 col-sm-12 col-xs-12" >
+			<div id="loginbox" style="margin-top:60px;margin-bottom:20px;" class="col-lg-8 col-lg-offset-2 col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1  col-xs-8 col-xs-offset-2">
+				<div style="padding-top:30px" class="panel-body " >
+					<div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+                    <form id="loginform" class="form-horizontal" role="form" style="margin-top:90px">
+                                    
+                            <div style="margin-bottom: 25px" class="input-group col-sm-offset-2 col-sm-8 col-md-6 col-md-offset-3">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                        <input id="n" type="text" class="form-control input-sm" name="n" required placeholder="Nombre completo">                                        
+                            </div>
+                                
+                            <div style="margin-bottom: 25px" class="input-group col-sm-offset-2 col-sm-8 col-md-6 col-md-offset-3">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-list-alt"></i></span>
+                                        <input id="c" type="text" class="form-control input-sm" name="c" required placeholder="CURP">                                        
+                            </div>
+									
+                            <div style="margin-bottom: 25px" class="input-group col-sm-offset-2 col-sm-8 col-md-6 col-md-offset-3">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+                                        <input id="e" type="text" class="form-control input-sm" name="e" required placeholder="Correo">                                        
+                            </div>
+									
+                            <div style="margin-bottom: 25px" class="input-group col-sm-offset-2 col-sm-8 col-md-6 col-md-offset-3">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                        <input id="p" type="password" class="form-control input-sm" name="c" required placeholder="Contraseña">
+                            </div>
+                                    
+                            <div style="margin-bottom: 25px" class="input-group col-sm-offset-2 col-sm-8 col-md-6 col-md-offset-3">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                        <input id="pc" type="password" class="form-control input-sm" name="cr" required placeholder="Confirmar contraseña">
+                            </div>
+						
+                            <div style="margin-bottom: 25px" class="input-group col-sm-offset-2 col-sm-8 col-md-6 col-md-offset-3">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
+                                        <input id="b" type="date" class="form-control input-sm" name="b" min="1940-04-01" required>
+										<span class="validity"></span>
+                            </div>
+							
+                            <div style="margin-bottom: 25px" class="input-group col-sm-offset-2 col-sm-8 col-md-6 col-md-offset-3">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-globe"></i></span>
+                                         <select class="form-control input-sm" required>
+										</select>
+                            </div>
+							
+                            <div style="margin-bottom: 25px" class="input-group col-sm-offset-2 col-sm-8 col-md-6 col-md-offset-3">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-map-marker"></i></span>
+                                        <select class="form-control input-sm" required>
+										</select>
+                            </div>
+                                
+                            <div class="input-group col-sm-offset-2 col-sm-8 col-md-6 col-md-offset-3">
+                                      <div class="checkbox">
+                                        <label>
+                                          <input id="acept_t_p" type="checkbox" name="remember" value="1"> <label for="checkbox">Acepto<a href="services.html">Términos de servicio</a> y <a href="services.html">Políticas de privacidad</a></label>
+                                        </label>
+                                      </div>
+                             </div>
+
+
+                            <div style="margin-top:10px" class="form-group">
+							<!-- Button -->
+								<div class="col-sm-8 col-sm-offset-2  col-md-6 col-md-offset-3 controls text-center">
+								<button type="button" id="registrar" style="width:100%" class="button registrar">Pre-registro</button>
+								</div>
+                            </div>
+							    
+                    </form>               
+				</div>  
+			</div>
+		</div>
+      <!-- preregister -->
+	  <h5 class="centered col-lg-8 col-lg-offset-2 col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1  col-xs-8 col-xs-offset-2">
+			Próximamente descarga nuestra APP solo para ususarios.<br></h5>
+		<div style="margin-bottom:50px;margin-top:20px" class="centered col-lg-8 col-lg-offset-2 col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1  col-xs-8 col-xs-offset-2">
+			<img style="height:60px;" src="http://localhost/Microprestamos123/assets/css/img/applestore.png" alt="Apple Store">
+			<img style="height:60px;" src="http://localhost/Microprestamos123/assets/css/img/playstore.png" alt="Play Store">
+		</div>
+		<div style="margin-bottom:50px" class="centered col-lg-8 col-lg-offset-2 col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1  col-xs-8 col-xs-offset-2">
+			<img  style="height:90px;" src="http://localhost/Microprestamos123/assets/css/img/p1.png" alt="Registro">
+			<img style="height:90px;" src="http://localhost/Microprestamos123/assets/css/img/p2.png" alt="Aprobación">
+			<img style="height:90px;" src="http://localhost/Microprestamos123/assets/css/img/p3" alt="Depósito">
+		</div>
+    </div>
+    <!-- container -->
+
+  <!-- JavaScript Libraries -->
+  <script src="http://localhost/Microprestamos123/assets/jquery/jquery.min.js"></script>
+  <script src="http://localhost/Microprestamos123/assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="http://localhost/Microprestamos123/assets/php-mail-form/validate.js"></script>
+  <script src="http://localhost/Microprestamos123/assets/chart/chart.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="http://localhost/Microprestamos123/assets/js/main.js"></script>
+
+</body>
+</html>
